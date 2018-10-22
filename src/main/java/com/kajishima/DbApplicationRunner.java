1589d8fb818a9b6f1377859750b4a7ec20d6a01b
Sachiko Kajishima <sachiko@kajishima.com>
2018-10-23 06:38:29 +0900

    @componentアノテーションを追加

--- src/main/java/com/kajishima/DbApplicationRunner.java
+++ src/main/java/com/kajishima/DbApplicationRunner.java
@@ -3,9 +3,11 @@ package com.kajishima;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import com.kajishima.service.ApplicationRunnerService;
 
+@Component
 public class DbApplicationRunner implements ApplicationRunner {
 
 	@Autowired
